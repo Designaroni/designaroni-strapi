dd69103359b3ec99172378a70a097c4f52ca9890
Ian Roberts <ian.roberts@designaroni.com>
2022-12-02 10:20:23 -0700

    adds middleware configuration for AWS bucket access

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,94 @@
-module.exports = [
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
-];
+module.exports = ({ env }) => {
+  if (env("NODE_ENV") === "production") {
+    return [
+      "strapi::errors",
+      {
+        name: "strapi::security",
+        config: {
+          contentSecurityPolicy: {
+            useDefaults: true,
+            directives: {
+              "connect-src": ["'self'", "https:"],
+              "img-src": [
+                "'self'",
+                "data:",
+                "blob:",
+                "dl.airtable.com",
+                `${env("AWS_BUCKET")}.s3.${env("AWS_REGION")}.amazonaws.com`,
+              ],
+              "media-src": [
+                "'self'",
+                "data:",
+                "blob:",
+                "dl.airtable.com",
+                `${env("AWS_BUCKET")}.s3.${env("AWS_REGION")}.amazonaws.com`,
+              ],
+              upgradeInsecureRequests: null,
+            },
+          },
+        },
+      },
+      "strapi::cors",
+      "strapi::poweredBy",
+      "strapi::logger",
+      "strapi::query",
+      "strapi::body",
+      "strapi::session",
+      "strapi::favicon",
+      "strapi::public",
+    ];
+  }
+
+  if (env("NODE_ENV") === "test") {
+    return [
+      "strapi::errors",
+      {
+        name: "strapi::security",
+        config: {
+          contentSecurityPolicy: {
+            useDefaults: true,
+            directives: {
+              "connect-src": ["'self'", "https:"],
+              "img-src": [
+                "'self'",
+                "data:",
+                "blob:",
+                "dl.airtable.com",
+                `${env("AWS_BUCKET")}.s3.${env("AWS_REGION")}.amazonaws.com`,
+              ],
+              "media-src": [
+                "'self'",
+                "data:",
+                "blob:",
+                "dl.airtable.com",
+                `${env("AWS_BUCKET")}.s3.${env("AWS_REGION")}.amazonaws.com`,
+              ],
+              upgradeInsecureRequests: null,
+            },
+          },
+        },
+      },
+      "strapi::cors",
+      "strapi::poweredBy",
+      "strapi::logger",
+      "strapi::query",
+      "strapi::body",
+      "strapi::session",
+      "strapi::favicon",
+      "strapi::public",
+    ];
+  }
+
+  return [
+    "strapi::errors",
+    "strapi::security",
+    "strapi::cors",
+    "strapi::poweredBy",
+    "strapi::logger",
+    "strapi::query",
+    "strapi::body",
+    "strapi::session",
+    "strapi::favicon",
+    "strapi::public",
+  ];
+};
